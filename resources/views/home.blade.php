b0150a9e413819ca353acf8e5ee55ff88c5386e1
Dimasafu <dimasauliafadhil@gmail.com>
2025-06-08 22:25:54 +0700

    Update home.blade.php
    Sudah bisa tapi masih pakai tailwind akan dibenarkan kembali menggunakan bootstrap terlebih dahulu

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
         <div class="container mx-auto px-4 py-20">
             <h1 class="text-5xl font-bold text-green-700 mb-4 animate-fade-in-down">Welcome to Florist Online</h1>
             <p class="text-lg text-gray-600 mb-6 animate-fade-in">Beautiful flowers delivered to your door</p>
-            <a href="{{ route('products.index') }}" class="bg-green-600 text-white px-6 py-3 rounded-lg shadow hover:bg-green-700 transition">
+            <a href="{{ route('products.index') }}" class="bg-green-600 text-black px-6 py-3 rounded-lg shadow hover:bg-green-700 transition">
                 Belanja Sekarang
             </a>
         </div>
